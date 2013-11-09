376688a3a64a5c853e67f0d763f16ceaacac7794
byron wallace <byron.wallace@gmail.com>
2013-11-09 13:01:52 -0500

    more work on the supervised learner

--- supervised_learner.py
+++ supervised_learner.py
@@ -7,16 +7,21 @@ from bilearn import bilearnPipeline
 
 
 class SupervisedLearner:
-    def __init__(self):
-        pass
+    def __init__(self, abstract_reader):
+        self.abstract_reader = abstract_reader
 
     def features_from_citations(self):
-        p = bilearnPipeline(text)
-        p.generate_features()
-        X = p.get_features(filter=lambda x: x["w[0]"].isdigit())
-        words = p.get_answers(filter=lambda x: x["w"].isdigit())
-
-        return X, words
+        X, y = [], []
+        for cit in self.abstract_reader:
+            abstract_text = cit["abstract"]
+            p = bilearnPipeline(text)
+            p.generate_features()
+            #filter=lambda x: x["w[0]"].isdigit()
+            X_i = p.get_features()
+            words = p.get_answers()
+            X.append(X_i)
+            y.append() ##????
+        return X, y
 
         
 
@@ -31,13 +36,27 @@ class LabeledAbstractReader:
 
     '''
     def __init__(self, path_to_data="data/drug_trials_in_cochrane_BCW.txt"):
-        # @TODO probably want to make this entire class an iterator,
-        # rather than loading everything into memory!
+        # @TODO probably want to read things in lazily, rather than
+        # reading everything into memory at once...
         self.abstracts = []
+        self.abstract_index = 0 # for iterator
         self.path_to_abstracts = path_to_data
         print "parsing data from {0}".format(self.path_to_abstracts)
         self.parse_abstracts()
+        self.num_citations = len(self.citation_d) 
         print "ok."
+
+
+    def __iter__(self):
+        self.abstract_index = 0
+        return self
+
+    def next(self):
+        if self.abstract_index >= self.num_citations:
+            raise StopIteration
+        else:
+            self.abstract_index += 1
+            return self.citation_d.values()[self.abstract_index-1]
 
     def _is_demarcater(self, l):
         '''
@@ -51,8 +70,8 @@ class LabeledAbstractReader:
     def _get_IDs(self, l):
         ''' Assumes l is a demarcating line; returns Biview and PMID ID's '''
         grab_id = lambda s : s.lstrip().split(" ")[1].strip()
-        biview_id, pmid_id = [grab_id(s) for s in l.split(";")]
-        return biview_id, pmid_id
+        biview_id, pmid = [grab_id(s) for s in l.split(";")]
+        return biview_id, pmid
 
     def _is_new_citation_line(self, l):
         return l.startswith("Abstract ")
@@ -66,9 +85,10 @@ class LabeledAbstractReader:
             for line in abstracts_file.readlines():
                 line = line.strip()
                 if self._is_demarcater(line):
-                    biview_id, pmid_id = self._get_IDs(line)
-                    self.citation_d[pmid_id] = {"abstract":cur_abstract, 
-                                                "Biview_id":biview_id}
+                    biview_id, pmid = self._get_IDs(line)
+                    self.citation_d[pmid] = {"abstract":cur_abstract, 
+                                                "Biview_id":biview_id,
+                                                "pubmed_id":pmid} # yes, redundant
                     in_citation = False
                 elif in_citation and line:
                     # then this is the abstract
