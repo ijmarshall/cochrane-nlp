c7f28ca01aed0a644d37e67cce705f973e541a11
Iain Marshall <iainjmarshall@gmail.com>
2013-11-16 22:23:34 +0000

    various updates
    -added test code
    -changed tag format to a list of tags in the functions dict
    -get_tags function which returns s list of {"w": word, "tags":
    tag_list} for all the words

--- taggedpipeline.py
+++ taggedpipeline.py
@@ -1,5 +1,6 @@
 
 import pipeline
+
 from nltk.tokenize import TreebankWordTokenizer
 from nltk.tokenize.punkt import *
 from collections import defaultdict, deque
@@ -149,9 +150,10 @@ class TaggedTextPipeline(pipeline.Pipeline):
 
             for (word, pos_tag), tag_list in izip(pos_tags, tags):
                 base_word_functions = {"w": word,
-                                       "p": pos_tag}
+                                       "p": pos_tag,
+                                       "tags": []}
                 for tag in tag_list:
-                    base_word_functions["xml-annotation-[%s]" % (tag, )] = True
+                    base_word_functions["tags"].append(tag)
 
                 base_sent_functions.append(base_word_functions)
             base_functions.append(base_sent_functions)
@@ -229,7 +231,10 @@ class TaggedTextPipeline(pipeline.Pipeline):
 
     def run_functions(self, show_progress=False):
         for i, sent_function in enumerate(self.functions):
-            words = {"BOW" + word["w"]: True for word in self.functions[i]}
+
+            # line below not used yet
+            # need to implement in Pipeline run_templates
+            # words = {"BOW" + word["w"]: True for word in self.functions[i]} 
 
             last_noun_index = 0
 
@@ -252,7 +257,10 @@ class TaggedTextPipeline(pipeline.Pipeline):
                             "punct": not any(c.isalnum() for c in word) # all punctuation}
                            }
                 self.functions[i][j].update(features)
-                self.functions[i][j].update(words)
+
+                # line below not used yet
+                # need to implement in Pipeline run_templates
+                # self.functions[i][j].update(words) 
 
                 # if pos is a noun, back fill the previous words
                 pos = self.functions[i][j]["p"]
@@ -264,18 +272,62 @@ class TaggedTextPipeline(pipeline.Pipeline):
             for k in range(last_noun_index, len(sent_function)):
                 self.functions[i][k]["next_noun"] = "END_OF_SENTENCE"
 
+    @pipeline.filters
+    def get_tags(self):
+
+
+        return [[{k: w[k] for k in ('w', 'tags')} for w in s] for s in self.get_base_functions()]
+
+
+
 
 def main():
-    print "go"
-    test = """
-    Early inflammatory lesions and bronchial hyperresponsiveness are characteristics of the respiratory distress in premature neonates and are susceptible to aggravation by assisted ventilation. We hypothesized that treatment with <tx4_a>inhaled salbutamol and <tx3_a>beclomethasone</tx3_a></tx4_a> might be of clinical value in the prevention of bronchopulmonary dysplasia (BPD) in ventilator-dependent premature neonates. The study was double-blinded and <tx1_a><tx2_a>placebo</tx1_a></tx2_a> controlled. We studied <n>173</n> infants of less than 31 weeks of gestational age, who needed ventilatory support at the 10th postnatal day. They were randomised to four groups and received either <tx1>placebo + placebo</tx1>, <tx2>placebo + salbutamol</tx2>, <tx3>placebo + beclomethasone</tx3> or <tx4>beclomethasone + salbutomol</tx4>, respectively for 28 days. The major criteria for efficacy were: diagnosis of BPD (with score of severity), mortality, duration of ventilatory support and oxygen therapy. The trial groups were similar with respect to age at entry (9.8-10.1 days), gestational age (27.6-27.8 weeks), birth weight and oxygen dependence. We did not observe any significant effect of treatment on survival, diagnosis and severity of BPD, duration of ventilatory support or oxygen therapy. For instance, the odds-ratio (95% confidence interval) for severe or moderate BPD were 1.04 (0.52-2.06) for <tx3_a><tx4_a>inhaled beclomethasone</tx3_a></tx4_a> and 1.54 (0.78-3.05) for <tx4_a>inhaled salbutamol</tx4_a>. This randomised prospective trial does not support the use of treatment with inhaled <tx3_a><tx4_a>beclomethasone</tx3_a>, salbutamol</tx4_a> or their combination in the prevention of BPD in premature ventilated neonates."
-
     
+    test_text = """
+    Early inflammatory lesions and bronchial hyperresponsiveness are characteristics of the respiratory distress
+    in premature neonates and are susceptible to aggravation by assisted ventilation. We hypothesized that
+    treatment with <tx4_a>inhaled salbutamol and <tx3_a>beclomethasone</tx3_a></tx4_a> might be of clinical
+    value in the prevention of bronchopulmonary dysplasia (BPD) in ventilator-dependent premature neonates. The
+    study was double-blinded and <tx1_a><tx2_a>placebo</tx1_a></tx2_a> controlled. We studied <n>173</n> infants
+    of less than 31 weeks of gestational age, who needed ventilatory support at the 10th postnatal day. They
+    were randomised to four groups and received either <tx1>placebo + placebo</tx1>, <tx2>placebo + salbutamol</tx2>
+    , <tx3>placebo + beclomethasone</tx3> or <tx4>beclomethasone + salbutomol</tx4>, respectively for 28 days.
+     The major criteria for efficacy were: diagnosis of BPD (with score of severity), mortality, duration of
+      ventilatory support and oxygen therapy. The trial groups were similar with respect to age at entry (9.8-10.1
+     days), gestational age (27.6-27.8 weeks), birth weight and oxygen dependence. We did not observe any
+    significant effect of treatment on survival, diagnosis and severity of BPD, duration of ventilatory support
+     or oxygen therapy. For instance, the odds-ratio (95% confidence interval) for severe or moderate BPD were 
+     1.04 (0.52-2.06) for <tx3_a><tx4_a>inhaled beclomethasone</tx3_a></tx4_a> and 1.54 (0.78-3.05) for <tx4_a>
+     inhaled salbutamol</tx4_a>. This randomised prospective trial does not support the use of treatment with 
+     inhaled <tx3_a><tx4_a>beclomethasone</tx3_a>, salbutamol</tx4_a> or their combination in the prevention of 
+     BPD in premature ventilated neonates.
     """
 
-    # test = "Test of text <n>tagger</n>"
 
-    b = TaggedTextPipeline(test)
+    p = TaggedTextPipeline(test_text)
+
+    tags = p.get_tags(flatten=True) # returns a list of tags
+
+    print "example of tags format (word3 20-25):"
+    print tags[25:30]
+
+    print
+    print "intervention 4 (words tagged tx4)"
+    print [w["w"] for w in tags if "tx4" in w["tags"]]
+
+    print
+    print "number of people randomised"
+    print [w["w"] for w in tags if "n" in w["tags"]]
+    
+    
+
+
+
+
+
+
+
+
 
 
 
