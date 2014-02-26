15a9f381cd2b872469bb7ce8792359c753f15767
Joel Kuiper <me@joelkuiper.eu>
2014-02-26 21:56:10 +0100

    It's official. I am a banana.

--- web/pipeline.py
+++ web/pipeline.py
@@ -29,7 +29,7 @@ class Pipeline(object):
                 page_str = " ".join(textNodes)
 
                 parsed.append({"str": page_str,
-                               "total": total,
+                               "length": total,
                                "intervals": interval_tree,
                                "ranges": ranges})
             else:
@@ -40,11 +40,11 @@ class Pipeline(object):
 
 class MockPipeline(Pipeline):
 
-    def random_annotations(self, nr_simulated, document_length=1000):
+    def random_annotations(self, nr_simulated, document_length):
         # Mock sentence level predictions by generating random annotations
         def randinterval(start, stop):
             lower = random.randrange(start, stop)
-            upper = random.randrange(lower, lower + 70) # 70 = average letters in sentence
+            upper = random.randrange(lower, lower + 100)
             return (lower, upper)
 
         # Simulate a dict like
@@ -57,24 +57,35 @@ class MockPipeline(Pipeline):
                    "domain_3": random.randint(-1, 1)} for i in range(nr_simulated)]
         return dict(zip(sentence_bounds, labels))
 
+
     def predict(self, input):
         parsed_pages = self.parse(input)
+
         # we store this because we want per document, not per page
-        page_bounds = [page["total"] for page in parsed_pages]
+        def accumulate(x, l=[0]): l[0] += x; return l[0];       
+        page_lengths = [page["length"] for page in parsed_pages]
+        total_length = map(accumulate, [0] + page_lengths)
+        print total_length
 
         # Mock document level predictions, this can be done on
         document_predictions = {"domain_1": 1, "domain_2": -1, "domain_3": 0}
 
         # Mock sentence level predictions
-        sentence_predictions = self.random_annotations(10, document_length=page_bounds[-1])
+        sentence_predictions = self.random_annotations(10, document_length=total_length[-1])
 
         # Now we need to get /back/ to the page and node indexes
         annotations = []
         for sentence_bound, labels in sentence_predictions.iteritems():
-            page_nr = next(i for i,v in enumerate(page_bounds) if v >= sentence_bound[0])
+            page_nr = next(i for i,v in enumerate(total_length) if v >= sentence_bound[0]) - 1
             page = parsed_pages[page_nr]
-            bound = (sentence_bound[0], sentence_bound[1] - 1)
-            nodes = [page["ranges"].index(x) for x in page["intervals"].overlap(sentence_bound)]
+            offset = total_length[page_nr]
+
+            bound = (sentence_bound[0] - offset, sentence_bound[1] - offset)
+
+            nodes = [page["ranges"].index(x) for x in page["intervals"].overlap(bound)]
+
+            print page_nr, offset, sentence_bound, bound, nodes, page["ranges"][0], page["ranges"][-1]
+
             annotations.append({
                 "page": page_nr,
                 "nodes": nodes,
