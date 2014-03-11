71d6cc97b669aa7a2ab9b7f0113384bb459c3c35
Joel Kuiper <me@joelkuiper.eu>
2014-03-11 14:34:04 +0100

    I really like Banyan
    ... I'll write my own interval / segment tree soon
    otherwise my CS training gets itchy

--- web/abstract_pipeline.py
+++ web/abstract_pipeline.py
@@ -1,43 +1,8 @@
-
-
 # I had to apply this patch: https://code.google.com/p/banyan/issues/detail?id=5
-# from banyan import *
+from banyan import *
 from abc import ABCMeta, abstractmethod
-
-class RangeList():
-    """
-    to duplicate the minimal function needed of an interval-tree
-    maintains a list of start, end tuples
-    and calculates overlaps
-    """
-    def __init__(self, intervals):
-        """
-        takes intervals = list of (start, end) tuples and sorts them
-        """
-        self.intervals = sorted(intervals)
-
-    def _is_overlapping(self, i1, i2):
-        return i2[0] < i1[1] and i1[0] < i2[1]
-
-
-    def overlap(self, bounds):
-        """
-        bounds = (start, end) tuple
-        returns all overlapping bounds
-        """
-        # TODO - we don't really need to iterate through *all* of these, since it's a sorted list
-        # we can stop early once no overlaps possible
-        # 
-        # Either this or don't bother sorting and keep this bit! (IM)
-        return [interval for interval in self.intervals if self._is_overlapping(interval, bounds)]
-
-    def overlap_indices(self, bounds):
-        """
-        return the 0 indexed positions of overlapping bounds
-        """
-        return [index for index, interval in enumerate(self.intervals) if self._is_overlapping(interval, bounds)]
-
-
+import logging
+log = logging.getLogger(__name__)
 
 class Pipeline(object):
     __metaclass__ = ABCMeta
@@ -60,23 +25,20 @@ class Pipeline(object):
                     total += len(txt)
                     ranges.append((start, total))
 
-                    total += 1 
+                    total += 1
                     # note that this +=1 aligns both for spaces added later between nodes *and* pages
                     # (since the ' '.join(nodes) does not leave a trailing space, but we add one anyway)
 
-                # interval_tree = SortedSet(ranges, key_type = (int, int), updator = OverlappingIntervalsUpdator)
-                interval_tree = RangeList(ranges)
+                interval_tree = SortedSet(ranges, key_type = (int, int), updator = OverlappingIntervalsUpdator)
                 page_str = " ".join(textNodes)
 
                 parsed.append({"str": page_str,
                                "length": total,
+                               "ranges": ranges,
                                "intervals": interval_tree})
-
             else:
-                parsed.append({})
-
-        
-
+                log.debug("Attempted to parse empty page ..." + str(idx))
+                continue
         return parsed
 
     def get_page_offsets(self, page_lengths):
@@ -96,19 +58,13 @@ class Pipeline(object):
 
         # Now we need to get /back/ to the page and node indexes
         annotations = []
-
-        
-
         for sentence_bound, labels in predictions.iteritems():
-
-
             page_nr = next((i for i, v in enumerate(total_length) if v > sentence_bound[0])) - 1
             page = parsed_input[page_nr]
             offset = total_length[page_nr]
 
             bound = (sentence_bound[0] - offset, sentence_bound[1] - offset)
-
-            nodes = page["intervals"].overlap_indices(bound)
+            nodes = [page["ranges"].index(x) for x in page["intervals"].overlap(bound)]
 
             annotations.append({
                 "page": page_nr,
