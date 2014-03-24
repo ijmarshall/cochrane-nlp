aeb3d665a06e42bf3657957890bb9a397fa8a048
Iain Marshall <iainjmarshall@gmail.com>
2014-03-24 23:20:26 +0000

    multitask doc model added

--- quality5.py
+++ quality5.py
@@ -11,9 +11,12 @@ import biviewer
 import codecs
 import yaml
 from unidecode import unidecode
+from nltk.corpus import stopwords
 
+from sklearn.externals import six
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction import DictVectorizer
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import KFold
 from sklearn.linear_model import SGDClassifier
@@ -25,6 +28,7 @@ import pprint
 import csv
 
 import difflib
+
 
 ############################################################
 #   
@@ -106,12 +110,15 @@ class RoBData:
                     doc_y[mapped_domain] = 1 if domain["RATING"] == "YES" else -1
                     # simplifying to LOW risk of bias = 1 *v* HIGH/UNKNOWN risk = -1
 
-                    if domain["QUOTES"]:
+                    if domain["QUOTES"] and not doc_level_only:
                         matcher.load_quotes(domain["QUOTES"])
                         sent_y[mapped_domain] = matcher.generate_y()
 
-            study_data = {"doc-text": pdf_text, "doc-y": doc_y,
+            if not doc_level_only:
+                study_data = {"doc-text": pdf_text, "doc-y": doc_y,
                           "sent-spans": matcher.sent_indices, "sent-y": sent_y}
+            else:
+                study_data = {"doc-text": pdf_text, "doc-y": doc_y}
 
             self.data.append(study_data)
 
@@ -265,7 +272,7 @@ class DataFilter(object):
         """
 
         # in the base class return *all* ids
-        return [i for i, doc in enumerate(data_instance.data)]
+        return [i for i, doc in enumerate(self.data_instance.data)]
 
 
     def Xy(self, doc_indices):
@@ -307,14 +314,37 @@ class SentFilter(DomainFilter):
         y = []
         for i in doc_indices:
             doc_i = self.data_instance.data[i]
-            for sent_i, sent_span in enumerate(doc_i["sent-spans"]):
-                X.append(doc_i["doc-text"][sent_span[0]:sent_span[1]])
-                if sent_i in doc_i["sent-y"][self.domain]:
-                    y.append(1)
-                else:
-                    y.append(-1)
-
+            X.append(doc_i["doc-text"])
+            y.append(doc_i["doc-y"][self.domain])
         return X, y
+
+class MultiTaskDocFilter(DataFilter):
+    """
+    for training a multi-task model (i.e. one model for all domains)
+    """
+
+    # subclasses _get_available_ids() default behaviour
+    # i.e. return all documents
+
+    def Xy(self, doc_indices):
+        raise NotImplemented("Xy not used in MultiTaskDocFilter - you probably want Xyi() for interaction terms")
+
+    def Xyi(self, doc_indices):
+        X = []
+        y = []
+        interactions = []
+
+        for i in doc_indices:
+            doc_i = self.data_instance.data[i]
+            for domain, judgement in doc_i["doc-y"].iteritems():                
+                if judgement == 0: # skip the blanks
+                    continue 
+                X.append(doc_i["doc-text"])
+                y.append(judgement)
+                interactions.append(domain)
+        return X, y, interactions
+
+
 
 
 ############################################################
@@ -332,9 +362,10 @@ class BinaryMetricsRecorder(object):
 
     """
 
-    METRIC_NAMES = ["f1", "precision", "recall", "accuracy"]
-    METRIC_FUNCTIONS = [sklearn.metrics.f1_score, sklearn.metrics.precision_score,
-                            sklearn.metrics.recall_score, sklearn.metrics.accuracy_score]
+    METRIC_NAMES = ["n", "f1", "precision", "recall", "accuracy"]
+    METRIC_FUNCTIONS = [lambda preds, test: len(preds),
+                        sklearn.metrics.f1_score, sklearn.metrics.precision_score,
+                        sklearn.metrics.recall_score, sklearn.metrics.accuracy_score]
 
 
     def __init__(self, title="Untitled", domains=["default"]):
@@ -347,12 +378,12 @@ class BinaryMetricsRecorder(object):
         """
         add a fold of data
         """
-        fold_metric = {metric_name: metric_fn(preds, test) for metric_name, metric_fn
+        fold_metric = {metric_name: metric_fn(test, preds) for metric_name, metric_fn
                     in zip (self.METRIC_NAMES, self.METRIC_FUNCTIONS)}
 
         fold_metric["domain"] = domain
         fold_metric["fold"] = len(self.fold_metrics[domain])
-
+        
         self.fold_metrics[domain].append(fold_metric)
 
     def _means(self, domain):
@@ -367,6 +398,7 @@ class BinaryMetricsRecorder(object):
 
         means["domain"] = domain
         means["fold"] = "mean"
+        means["n"] = np.sum(summaries["n"]) # overwrite with the sum
 
         return means
 
@@ -390,6 +422,179 @@ class BinaryMetricsRecorder(object):
 
 ############################################################
 #   
+#   Vectorizer
+#
+############################################################
+
+
+
+class ModularCountVectorizer():
+    """
+    Similar to CountVectorizer from sklearn, but allows building up
+    of feature matrix gradually, and adding prefixes to feature names
+    (to identify interaction terms)
+    """
+    STOP_WORDS = set(stopwords.words('english'))
+    SIMPLE_WORD_TOKENIZER = re.compile("[a-zA-Z]{2,}") # regex of the rule used by sklearn CountVectorizer
+
+
+    def __init__(self, *args, **kwargs):
+        self.data = []
+        self.vectorizer = DictVectorizer(*args, **kwargs)
+
+    def _transform_X_to_dict(self, X, prefix=None, weighting=1):
+        """
+        makes a list of dicts from a document
+        1. word tokenizes
+        2. creates {word1:1, word2:1...} dicts
+        (note all set to '1' since the DictVectorizer we use assumes all missing are 0)
+        """
+        return [self._dict_from_word_list(
+            self._word_tokenize(document, prefix=prefix), weighting=1) for document in X]
+
+    def _word_tokenize(self, text, prefix=None, stopword=True):
+        """
+        simple word tokenizer using the same rule as sklearn
+        punctuation is ignored, all 2 or more letter characters are a word
+        """
+
+
+        stop_word_list = self.STOP_WORDS if stopword else set()
+
+        if prefix:
+            return [prefix + word.lower() for word in self.SIMPLE_WORD_TOKENIZER.findall(text) 
+                        if not word.lower() in stop_word_list]
+        else:
+            return [word.lower() for word in self.SIMPLE_WORD_TOKENIZER.findall(text) 
+                        if not word.lower() in stop_word_list]
+
+
+    def _dict_from_word_list(self, word_list, weighting=1):
+        return {word: weighting for word in word_list}
+
+    def _dictzip(self, dictlist1, dictlist2):
+        """
+        zips together two lists of dicts of the same length
+        """
+        # checks lists must be the same length
+        if len(dictlist1) != len(dictlist2):
+            raise IndexError("Unable to combine featuresets with different number of examples")
+
+        output = []
+
+        for dict1, dict2 in zip(dictlist1, dictlist2):
+            output.append(dict(dict1.items() + dict2.items()))
+            # note that this *overwrites* any duplicate keys with the key/value from dictlist2!!
+
+        return output
+
+    def transform(self, X, prefix=None):
+        # X is a list of document strings
+        # word tokenizes each one, then passes to a dict vectorizer
+        dict_list = self._transform_X_to_dict(X, prefix=prefix)
+        return self.vectorizer.transform(dict_list)
+
+    def fit_transform(self, X, prefix=None, max_features=None, low=None):
+        # X is a list of document strings
+        # word tokenizes each one, then passes to a dict vectorizer
+        dict_list = self._transform_X_to_dict(X, prefix=prefix)
+        X = self.vectorizer.fit_transform(dict_list)
+
+        if max_features is not None or low is not None:
+            X, removed = self._limit_features(X.tocsc(), 
+                        self.vectorizer.vocabulary_, low=low, limit=max_features)
+            print "pruned %s features!" % len(removed)
+            X = X.tocsc()
+
+        return self.vectorizer.fit_transform(dict_list)
+
+    def get_feature_names(self):
+        return self.vectorizer.get_feature_names()
+
+
+    def builder_clear(self):
+        self.builder = []
+        self.builder_len = 0
+
+    def builder_add_docs(self, X, prefix = None, weighting=1):
+        #pdb.set_trace()
+        if not self.builder:
+            self.builder_len = len(X)
+            self.builder = self._transform_X_to_dict(X, prefix=prefix, weighting=weighting)
+        else:
+            X_dicts = self._transform_X_to_dict(X, prefix=prefix, weighting=weighting)
+            self.builder = self._dictzip(self.builder, X_dicts)
+
+    def builder_add_interaction_features(self, X, interactions, prefix=None):
+        if prefix is None:
+            raise TypeError('Prefix is required when adding interaction features')
+
+        doc_list = [(sent if interacting else "") for sent, interacting in zip(X, interactions)]
+        self.builder_add_docs(doc_list, prefix)
+
+        
+    def builder_fit_transform(self, max_features=None, low=2):
+        X = self.vectorizer.fit_transform(self.builder)
+        if max_features is not None or low is not None:
+            X, removed = self._limit_features(X.tocsc(), 
+                        self.vectorizer.vocabulary_, low=low, limit=max_features)
+            print "pruned %s features!" % len(removed)
+            X = X.tocsc()
+
+        return X #self.vectorizer.fit_transform(self.builder)
+
+    def builder_transform(self):
+        return self.vectorizer.transform(self.builder)   
+
+
+    def _limit_features(self, cscmatrix, vocabulary, high=None, low=None,
+                        limit=None):
+        """Remove too rare or too common features.
+
+        Prune features that are non zero in more samples than high or less
+        documents than low, modifying the vocabulary, and restricting it to
+        at most the limit most frequent.
+
+        This does not prune samples with zero features.
+        """
+        if high is None and low is None and limit is None:
+            return cscmatrix, set()
+
+        # Calculate a mask based on document frequencies
+        dfs = self._document_frequency(cscmatrix)
+        mask = np.ones(len(dfs), dtype=bool)
+        if high is not None:
+            mask &= dfs <= high
+        if low is not None:
+            mask &= dfs >= low
+        if limit is not None and mask.sum() > limit:
+            # backward compatibility requires us to keep lower indices in ties!
+            # (and hence to reverse the sort by negating dfs)
+            mask_inds = (-dfs[mask]).argsort()[:limit]
+            new_mask = np.zeros(len(dfs), dtype=bool)
+            new_mask[np.where(mask)[0][mask_inds]] = True
+            mask = new_mask
+
+        new_indices = np.cumsum(mask) - 1  # maps old indices to new
+        removed_terms = set()
+        for term, old_index in list(six.iteritems(vocabulary)):
+            if mask[old_index]:
+                vocabulary[term] = new_indices[old_index]
+            else:
+                del vocabulary[term]
+                removed_terms.add(term)
+        kept_indices = np.where(mask)[0]
+
+        return cscmatrix[:, kept_indices], removed_terms
+
+    def _document_frequency(self, X):
+        """Count the number of non-zero values for each feature in csc_matrix X."""
+        return np.diff(X.indptr)
+
+
+
+############################################################
+#   
 #   experiments
 #
 ############################################################
@@ -401,11 +606,12 @@ class BinaryMetricsRecorder(object):
 def simple_model_test(data_filter=DocFilter):
 
     dat = RoBData(test_mode=False)
-    dat.generate_data()
+    dat.generate_data(doc_level_only=True)
 
 
     metrics = BinaryMetricsRecorder(domains=dat.CORE_DOMAINS)
 
+    stupid_metrics = BinaryMetricsRecorder(domains=dat.CORE_DOMAINS)
 
     for domain in dat.CORE_DOMAINS:
 
@@ -423,7 +629,7 @@ def simple_model_test(data_filter=DocFilter):
         for train, test in kf:
 
             X_train_d, y_train = docs.Xy(uids[train])
-            X_test_d, y_test = docs.Xy(uids[train])
+            X_test_d, y_test = docs.Xy(uids[test])
 
             vec = CountVectorizer()
 
@@ -436,10 +642,89 @@ def simple_model_test(data_filter=DocFilter):
 
             metrics.add_preds_test(y_preds, y_test, domain=domain)
 
+            stupid_metrics.add_preds_test([1] * len(y_test), y_test, domain=domain)
+
     metrics.save_csv('test_output.csv')
+
+    stupid_metrics.save_csv('stupid_output.csv')
             
     
 
+
+def multitask_test():
+
+    dat = RoBData(test_mode=False)
+    dat.generate_data(doc_level_only=True)
+
+
+    metrics = BinaryMetricsRecorder(domains=dat.CORE_DOMAINS)
+
+    train_docs = MultiTaskDocFilter(dat)
+    train_uids = np.array(train_docs.available_ids)
+
+    tuned_parameters = {"alpha": np.logspace(-4, -1, 10)}
+    clf = GridSearchCV(SGDClassifier(loss="log", penalty="L2"), tuned_parameters, scoring='f1')
+
+    no_studies = len(train_uids)
+
+    kf = KFold(no_studies, n_folds=5, shuffle=False)
+
+    for train, test in kf:
+
+        print "new fold!"
+
+        X_train_d, y_train, i_train = train_docs.Xyi(train_uids[train])
+
+        # build up test data
+        interactions = {domain:[] for domain in dat.CORE_DOMAINS}
+        for doc_text, doc_domain in zip(X_train_d, i_train):
+            for domain in dat.CORE_DOMAINS:
+                if domain == doc_domain:
+                    interactions[domain].append(doc_text)
+                else:
+                    interactions[domain].append("")
+
+        # add to vec
+        vec = ModularCountVectorizer()
+        vec.builder_clear()
+
+        vec.builder_add_docs(X_train_d) # add base features
+
+        for domain in dat.CORE_DOMAINS:
+            vec.builder_add_docs(interactions[domain], prefix=domain+"-i-") # then add interactions
+
+        X_train = vec.builder_fit_transform()
+        
+
+        clf.fit(X_train, y_train)
+
+
+        for domain in dat.CORE_DOMAINS:
+
+            test_docs = DocFilter(dat, domain=domain) # test on regular doc model
+            domain_uids = np.array(test_docs.available_ids)
+
+            test_uids = np.intersect1d(train_uids[test], domain_uids)
+
+            X_test_d, y_test = test_docs.Xy(test_uids)
+
+            # build up test vector
+
+            vec.builder_clear()
+            vec.builder_add_docs(X_test_d) # add base features
+            vec.builder_add_docs(X_test_d, prefix=domain+'-i-') # add interactions
+
+            X_test = vec.builder_transform()
+
+            y_preds = clf.predict(X_test)
+
+            metrics.add_preds_test(y_preds, y_test, domain=domain)
+
+
+    metrics.save_csv('multitask_test.csv')
+
+
+            
 
 
 
@@ -453,6 +738,7 @@ def simple_model_test(data_filter=DocFilter):
 
 if __name__ == '__main__':
     simple_model_test(data_filter=DocFilter)
+    # multitask_test()
     
 
 
