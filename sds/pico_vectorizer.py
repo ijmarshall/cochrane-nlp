699f2743261845eb347050c30a4d9b3345d5bba5
bwallace <byron.wallace@gmail.com>
2015-02-25 14:34:22 -0600

    adding vectorizer

---
+++ sds/pico_vectorizer.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+
+import sklearn 
+from sklearn.feature_extraction.text
+from scipy.sparse import lil_matrix, csc_matrix
+
+
+
+
+class PICO_vectorizer:
+
+    def __init__(self):
+        pass 
+
+    def fit(self, sentences):
+        pass 
+
+    def transform(self, sentences):
+        m = len(sentences)
+        feature_matrix = csc_matrix()
+        #pass
+
