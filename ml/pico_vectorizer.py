0680b1b839fdf3f38e8e45c5ef435c9ef8e45926
Iain Marshall <mail@ijmarshall.com>
2015-03-24 10:25:12 -0500

    add extra features facility

--- ml/pico_vectorizer.py
+++ ml/pico_vectorizer.py
@@ -5,6 +5,8 @@ from scipy.sparse import lil_matrix, csc_matrix
 import numpy as np
 import scipy as sp
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import normalize
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
@@ -20,6 +22,7 @@ class PICO_vectorizer:
 
     def __init__(self):
         self.vectorizer = CountVectorizer(min_df=3, max_features=50000, ngram_range=(1, 2))
+        self.dict_vectorizer = DictVectorizer() # used to pass non-textual features
         self.drugbank = Drugbank()
 
     def is_number(self,num):
@@ -29,10 +32,15 @@ class PICO_vectorizer:
         except ValueError:
             return False
 
-    def fit(self, sentences):
+    def fit(self, sentences, extra_features=None):
+        print "fitting sentence dict"
         self.vectorizer.fit(sentences)
 
-    def transform(self,sentences):
+        print "fitting additional features (a list of dicts is available)"
+        if extra_features is not None:
+            self.dict_vectorizer.fit(extra_features)
+
+    def transform(self, sentences, extra_features=None):
 
         print "ok, extracting text features"
         X = self.vectorizer.transform(sentences)
@@ -43,16 +51,24 @@ class PICO_vectorizer:
         #extract numeric features from sentences
         X_numeric = self.extract_numeric_features(sentences)
 
-        print "combining features"
-        #now combine feature sets.
-        feature_matrix = sp.sparse.hstack((X_text, X_numeric)).tocsr()
+        if extra_features:
+            print "ok, extracting additional features (a list of dicts is available)"
+            X_extra_features = self.dict_vectorizer.transform(extra_features)
+            print "combining features"
+            #now combine feature sets.
+            feature_matrix = sp.sparse.hstack((X_text, X_numeric, X_extra_features)).tocsr()
+
+        else:
+            print "combining features"
+            #now combine feature sets.
+            feature_matrix = sp.sparse.hstack((X_text, X_numeric)).tocsr()
         #returning the vectorizer and feature matrix
         #need to figure out if we need to return vectorizer
         return feature_matrix
     
-    def fit_transform(self, sentences):
-        self.fit(sentences)
-        f_matrix = self.transform(sentences)
+    def fit_transform(self, sentences, extra_features=None):
+        self.fit(sentences, extra_features)
+        f_matrix = self.transform(sentences, extra_features)
         return f_matrix
     
     def extract_numeric_features(self,sentences, normalize_matrix=False):
