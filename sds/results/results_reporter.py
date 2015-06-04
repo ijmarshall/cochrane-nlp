a2190021b3067d6a4bbc0d3f5bf0a4a572a62002
bwallace <byron.wallace@gmail.com>
2015-06-04 09:50:18 -0500

    more results inspection

--- sds/results/results_reporter.py
+++ sds/results/results_reporter.py
@@ -4,6 +4,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+import pandas as pd
+
 import numpy as np 
 
 from sklearn import metrics
@@ -11,6 +13,53 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 
 fields = ["CHAR_PARTICIPANTS", "CHAR_INTERVENTIONS", "CHAR_OUTCOMES"]
+
+_neg_to_0 = lambda x : 0 if x <= 0 else 1.0
+
+
+# e.g., 
+# sds_study_scores1, sds_study_scores2, sds_sorted_results = results_reporter.individual_runs("1433386342-results-sds_all_sentence_scores.txt")
+def individual_runs(sent_results_path):
+    study_scores1, study_scores2 = [], []
+    sorted_results = []
+
+    per_study_results = _split_by_pmids(sent_results_path)
+    for pmid, results in per_study_results.items():
+        sorted_results_for_pmid = results.sort("raw pred", ascending=False)
+        #pdb.set_trace()
+        #sorted_results[pmid] = sorted_results_for_pmid
+        sorted_results.append(sorted_results_for_pmid)
+        l1_exists = _neg_to_0(sorted_results_for_pmid[:3]["lbl1"].max())
+        l2_exists = _neg_to_0(sorted_results_for_pmid[:3]["lbl2"].max())
+
+        study_scores1.append(l1_exists)
+        study_scores2.append(l2_exists)
+
+    return study_scores1, study_scores2, sorted_results
+
+def dump_false_positives(sent_results_path, lbl_col="lbl1"):
+    per_study_results = _split_by_pmids(sent_results_path)
+    false_positives_str = [["pmid", "sentence"]]
+
+    for pmid, results in per_study_results.items():
+        sorted_results_for_pmid = results.sort("raw pred", ascending=False)
+        indices = sorted_results_for_pmid.index
+        # only  output sentences for entire studies with '0' metric!
+        if sorted_results_for_pmid[lbl_col][:3].max() < 1: 
+            for i in indices[:3]:                  
+                false_positives_str.append(
+                    ["%s" % sorted_results_for_pmid["pmid"][i],
+                     "%s" % sorted_results_for_pmid["sentence"][i]])
+
+    with open(sent_results_path.replace(".txt", "_false_positives.csv"), 'wb') as outf:
+        csv_w = csv.writer(outf)    
+        csv_w.writerows(false_positives_str)
+
+def _split_by_pmids(sent_results_path):
+    all_sent_scores = pd.read_csv(sent_results_path)
+    grouped = all_sent_scores.groupby("pmid")
+    by_pmid = dict(list(grouped))
+    return by_pmid
 
 
 def generate_plots(nguyen_path, sds_path):
