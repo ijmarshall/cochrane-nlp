dff3fb2f45ca1b82a7f0afa29be59988c2fb5af4
Edward Banner <edward.banner@gmail.com>
2016-02-24 10:02:09 -0600

    Push character language model code
    This is just a dump before I move onto other things

--- experiments/ct.gov/abstracts_char_lm/support.py
+++ experiments/ct.gov/abstracts_char_lm/support.py
@@ -7,7 +7,15 @@
 
 import numpy as np
 
-def sample_sentences(model, num_abstracts, vocab_dim, char2idx, idx2char, sent_len=100):
+def sample(a, temperature=1.0):
+    """Helper function to sample an index from a probability array"""
+
+    a = np.log(a) / temperature
+    a = np.exp(a) / np.sum(np.exp(a))
+
+    return np.argmax(np.random.multinomial(1, a, 1))
+
+def sample_sentences(model, num_abstracts, vocab_dim, char2idx, idx2char, num_seqs, sent_len=1, temperature=.5):
     """Sample a sequence from the char nn
     
     Since we're stuck feeding in 2,093 samples at each iteration, we might as
@@ -20,17 +28,20 @@ def sample_sentences(model, num_abstracts, vocab_dim, char2idx, idx2char, sent_l
     """
     model.reset_states()
     
-    X = np.zeros(shape=[num_abstracts, sent_len], dtype=np.int)
-    X[:, 0] = char2idx['START']
+    X = np.zeros(shape=[num_abstracts, 1], dtype=np.int)
+    X[:, 0] = char2idx['START'] # forget about the rest after `num_abstracts`
+
+    ys = np.zeros([num_seqs, sent_len])
     
     for t in range(sent_len):
         # Predict distribution over next character
         #
         probs = model.predict(X, batch_size=num_abstracts)
-        probs /= probs.sum(axis=1, keepdims=True) # numerical errors sometimes make np.random.choice complain
-        
-        for i, sample in enumerate(X):
-            predicted_idx = np.random.choice(vocab_dim, p=probs[i])
-            X[i, t] = predicted_idx
+        for i in range(num_seqs): # only update the first `num_abstracts` sequences
+            predicted_idx = sample(probs[i, 0], temperature)
+            # predicted_idx = np.argmax(probs[i, 0])
+            ys[i, t] = predicted_idx
 
-    return X
+        X[:, 0] = predicted_idx # update X so the next char we feed in is the one predicted by the rnn
+
+    return ys
